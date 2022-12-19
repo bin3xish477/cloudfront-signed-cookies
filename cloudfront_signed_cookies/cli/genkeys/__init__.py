2d43720adff5e9a5ce03848a594bda5b10e6d7f4
Alexis Rodriguez <arodriguez99@protonmail.com>
2022-12-19 01:24:18 -0500

    finishing genkeys CLI command which generates and saves private/public PEM-formatted keys

--- cloudfront_signed_cookies/cli/genkeys/__init__.py
+++ cloudfront_signed_cookies/cli/genkeys/__init__.py
@@ -1,12 +1,33 @@
 import click
-import logging
+from cryptography.hazmat.primitives import serialization
 
 from cryptography.hazmat.primitives.asymmetric import rsa
 
-logging.basicConfig(level=logging.DEBUG)
-
 @click.command(context_settings={"ignore_unknown_options": True})
-@click.option("--key-size", "-s", default=2048)
-@click.pass_context
-def genkeys(ctx: click.Context, key_size: int) -> None:
-    pass
+@click.option("--key-size", "-s", default=2048, help="the key size in bits, default=2048")
+def genkeys(key_size: int) -> None:
+    with (
+        open('private_key.pem', mode='wb') as priv_key_file,
+        open('public_key.pem', mode='wb') as pub_key_file
+    ):
+        priv_key: rsa.RSAPrivateKey = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=key_size
+        )
+        # writing private PEM-formatted key
+        pub_key: rsa.RSAPublicKey = priv_key.public_key()
+        priv_pem = priv_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+        priv_key_file.write(priv_pem)
+        print(f"==> Saved private key to 'private_key.pem'")
+        # writing public PEM-formatted key
+        pub_pem = pub_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        pub_key_file.write(pub_pem)
+        print(f"==> Saved public key to 'public_key.pem'")
+
