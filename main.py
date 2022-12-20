7670769a5fbcafc9b83128b9cf0e09e3007bf74f
Alexis Rodriguez <arodriguez99@protonmail.com>
2022-12-20 14:30:14 -0500

    adding pre-commit script to execute black automatically and adding parameter argument to sign call

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from cloudfront_signed_cookies.signer import Signer
 
 if __name__ == "__main__":
-    signer = Signer(cloudfront_key_id="46858301-6fdb-4645-a522-d09b5dea27a5", priv_key_file="./certs/private_key.pem")
+    signer = Signer(
+        cloudfront_key_id="46858301-6fdb-4645-a522-d09b5dea27a5",
+        priv_key_file="./certs/private_key.pem",
+    )
 
     cookies: dict = signer.generate_cookies(
         Resource="https://s3.amazonaws.com/somefile.txt",
